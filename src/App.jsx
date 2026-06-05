a14c1c379a44dc2186d342fa1c41c7e946059d7a
Abinath <abinath2105@gmail.com>
2026-06-05 12:46:22 +0530

    Updated Code with whatsapp intergration

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,45 @@
-import { Routes, Route } from "react-router-dom";
+// import { Routes, Route } from "react-router-dom";
 
-import Header from "./components/pages/Header";
-import Home from "./components/pages/Home";
+// import Header from "./components/pages/Header";
+// import Home from "./components/pages/Home";
+
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <div className="app-layout">
+
+//       <Header />
+
+//       <main className="main-content">
+//         <Routes>
+//           <Route
+//             path="/"
+//             element={<Home />}
+//           />
+//         </Routes>
+//       </main>
+
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
 
 import "./App.css";
+
+import { Routes, Route } from "react-router-dom";
+import { FaWhatsapp } from "react-icons/fa";
+import Header from "./components/pages/Header";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
     <div className="app-layout">
-
       <Header />
 
       <main className="main-content">
@@ -20,6 +51,21 @@ function App() {
         </Routes>
       </main>
 
+      {/* Floating WhatsApp */}
+      <a
+        href="https://wa.me/919176465416"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="floating-whatsapp"
+      >
+        <span className="whatsapp-tooltip">
+          Chat With Us
+        </span>
+
+        <span className="whatsapp-ripple"></span>
+
+        <FaWhatsapp />
+      </a>
     </div>
   );
 }
